b26510f7c90058f24afeda909b80ef05d569d256
Tim Sebring <tim.sebring@gmail.com>
2014-10-09 23:12:25 -0400

    Adding hello world programs

---
+++ calc.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+
+
+int main() {
+  cout << "7+3 = " << 7+3 << endl;
+
+  return 0;
+}
---
+++ game_over.cpp
@@ -0,0 +1,10 @@
+// This is a hello world-ish type program
+
+#include <iostream>
+
+using namespace std;
+
+int main() {
+  cout << "Game Over!" << endl;
+  return 0;
+}
